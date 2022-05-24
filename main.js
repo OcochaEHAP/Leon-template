2f2583d4dc2672a0c8d4f0108b7a34b41ba5402b
OkochaEH <oussamaayachefff@gmail.com>
2022-05-24 21:18:31 +0100

    add some cool features and some js

--- main.js
+++
@@ -1,6 +0,0 @@
-/*
-  Hi evryone ,
-  I hope you are doing great
-  ok i have a very basic knowlege of javascript i create this file for practicing, because i am learniig git & github from yoututbe "elzro web school"
-  but do not gues i am stupid , i know python
-*/
---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+  Hi evryone ,
+  I hope you are doing great
+  ok i have a very basic knowlege of javascript i create this file for practicing, because i am learniig git & github from yoututbe "elzro web school"
+  but do not gues i am stupid , i know python
+  >
+    this is me again after ji learned js, I'm going to update this template using advanced Css and js 
+*/
+// variables
+let links = document.querySelector(".links");
+let navbar = document.querySelector(".navbar");
+let navbarIcon = document.querySelector(".icon");
+// variables
+links.addEventListener("click", () => {
+  links.classList.toggle("active");
+});
